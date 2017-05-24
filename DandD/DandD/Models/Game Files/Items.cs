6605b498480a1134d648009a232d2e0775187bb0
trentc1 <trentc1@seattleu.edu>
2017-05-23 17:26:51 -0700

    Fixed api pull

--- DandD/DandD/Models/Game Files/Items.cs
+++ DandD/DandD/Models/Game Files/Items.cs
@@ -6,12 +6,15 @@ namespace DandD.Models.Game_Files
 {
     public class Items: BaseDataObject
     {
-        public string FilePath { get; set; }
+        public string Image { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string AttribMod { get; set; }
         public string BodyPart { get; set; }
-        public int Useage { get; set; }
+        public int Usage { get; set; }
         public string Creator { get; set; }
+        public int Values { get; set; }
+        public int Tier { get; set; }
         public int Str { get; set; }
         public int Dex { get; set; }
         public int Speed { get; set; }
